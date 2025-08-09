3c0fecb2b08879530def8cebf86094482f28925b
Nodari-0 <nodarirusishvililinkedin@gmail.com>
2025-08-09 15:26:15 +0200

    done

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,33 @@
-function Header() {
-  return (
-    <>
-      <nav>
-        <div className="logo">
-          <img
-            src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/discord-logo-white.png"
-            alt="Logo"
-          />
-        </div>
-        <div className="links">
-          <img src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/menu-icon.png" alt="" />
-        </div>
-      </nav>
-    </>
-  );
+import type { FC } from 'react';
+
+interface HeaderProps {
+  logoSrc?: string;
+  menuIconSrc?: string;
+  onMenuClick?: () => void;
+  className?: string;
 }
+
+const Header: FC<HeaderProps> = ({
+  logoSrc = "https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/discord-logo-white.png",
+  menuIconSrc = "https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/menu-icon.png",
+  onMenuClick,
+  className = ''
+}) => {
+  return (
+    <nav className={className}>
+      <div className="logo">
+        <img src={logoSrc} alt="Logo" />
+      </div>
+      <div className="links">
+        <img 
+          src={menuIconSrc} 
+          alt="Menu" 
+          onClick={onMenuClick}
+          style={{ cursor: onMenuClick ? 'pointer' : 'default' }}
+        />
+      </div>
+    </nav>
+  );
+};
 
 export default Header;

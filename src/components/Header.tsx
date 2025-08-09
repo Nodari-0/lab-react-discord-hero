02bba1bce3a58186bdfde07a60c95903cd588486
Nodari-0 <nodarirusishvililinkedin@gmail.com>
2025-08-09 13:11:15 +0200

    done

---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+function Header() {
+  return (
+    <>
+      <nav>
+        <div className="logo">
+          <img
+            src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/discord-logo-white.png"
+            alt="Logo"
+          />
+        </div>
+        <div className="links">
+          <img src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/menu-icon.png" alt="" />
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Header;
---
+++ src/components/Main.tsx
@@ -0,0 +1,30 @@
+function Main() {
+  return (
+    <>
+      <main className="main-content">
+        <h1>Imagine a place...</h1>
+        <p className="description">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
+          quam ipsum fugiat saepe modi ullam laboriosam necessitatibus
+          praemus, pariatur quas earum iusto eligendi
+          enim? Debitis it numquam.ugiat saepe modi ullam laboriosam necessitatibus
+          praemus, pariatur quas earum iusto eligendi
+          enim? Debitis it numquam.
+        </p>
+        <div className="buttons">
+          <button className="btn btn-primary">Download for Windows</button>
+          <button className="btn btn-secondary">
+            Open Discord in your browser
+          </button>
+        </div>
+      </main>
+      <img className="background-image"
+        src="
+    https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/discord-background.png"
+        alt=""
+      />
+    </>
+  );
+}
+
+export default Main;
